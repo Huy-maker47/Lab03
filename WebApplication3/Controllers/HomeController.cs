e52d6b157dde6c3aae29d29378d427e95278750f
PC <PC@LAPTOP-FUENDMBJ>
2025-10-04 17:47:22 +0700

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult bang()
+        {
+            return View();
+        }
+        public ActionResult baif4()
+        {
+            return View();
+        }
+        public ActionResult baif5()
+        {
+            return View();
+        }
+        public ActionResult bang3()
+        {
+            return View();
+        }
+        public ActionResult BANG2()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ WebApplication3/Views/Home/jl.js
@@ -0,0 +1,33 @@
+﻿
+// Lấy giá trị của một input : bỏ khoảng trắng 2 đầu
+function getValue(id) {
+    return document.getElementById(id).value.trim();
+}
+// Hiển thị lỗi
+function showError(key, mess) {
+    document.getElementById(key + '_error').innerHTML = mess;
+}
+// kiểm tra lỗi
+function validate() {
+    var flag = true;
+    // 1 username
+    var username = getValue('username');
+    if (username == '' || username.length < 5 || !/^[a-zA-Z0-9]+$/.test(username)) {
+        flag = false;
+        showError('username', 'Vui lòng kiểm tra lại Username');
+    }
+    // 2. password
+    var password = getValue('password');
+    var repassword = getValue('repassword');
+    if (password == '' || password.length < 8) {
+        flag = false;
+        showError('password', 'Vui lòng kiểm tra lại Password');
+    }
+    // 3. repassword
+    var repassword = getValue('repassword');
+    if (password != repassword) {
+        flag = false;
+        showError('repassword', 'Vui lòng kiểm tra nhập lại Password');
+    }
+    return flag;
+}
